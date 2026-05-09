52ff891606ddc7174c9299c28a72ed1e77e2b9e4
Youssef Nader <ynader2017@gmail.com>
2026-05-09 08:50:57 +0300

    u

--- src/axios/custom.ts
+++ src/axios/custom.ts
@@ -5,17 +5,18 @@ const customFetch = axios.create({
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
-    "ngrok-skip-browser-warning": "true", // ⬅️ أضف ده
+    "ngrok-skip-browser-warning": "true",
   },
 });
 
-// إضافة التوكن تلقائياً لكل الطلبات
 customFetch.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  // تأكد أن الـ header موجود في كل طلب
+  config.headers["ngrok-skip-browser-warning"] = "true";
+  return config;
 });
 
 export default customFetch;
